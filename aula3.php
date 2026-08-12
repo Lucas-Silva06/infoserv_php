9d48415abcc86faaa443c599986021ad8e2d0ab3
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-11 22:01:45 -0300

    fatorial e numero par

--- aula3.php
+++ aula3.php
@@ -7,7 +7,7 @@ switch ($nota) {
     echo "Aprovado";
     break;
     
-    case "$nota >= 5":
+    case $nota >= 5:
         echo "Recuperação";
         break;
         
@@ -28,5 +28,34 @@ else {
     echo "Perfil desconhecido";
 }
 
+/** 
+ * Utilizar o operador modulo %
+ * Para descobrir se o numero informado pelo usuario é par
+ * Se for par exibir é par e o número lido, se não exibir impar e o número lido
+ */
+ 
+echo "<br>";
 
+$numero = 10;
+$resto = $numero %2;
 
+if ($resto == 0) {
+    echo "É par:". $numero;
+    echo "<br>";
+}
+else {
+    echo "É impar:" . $numero;
+}
+
+/** 
+ * Calcular o fatorial de (5!)
+ * 5 * 4 *3 *2 * 1 = 120;
+ * utilizar laço de repetição
+ */
+
+$numero = 5;
+$fatorial = 0;
+
+for ($numero = 5; $numero > 0; $numero-- ) {
+    echo $fatorial = $numero * 5;
+}
